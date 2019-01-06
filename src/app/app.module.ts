2ec1d7aee38725f5f98343a7a65bd641ce6d22a1
Sarath Bodepudi <sarathbabubodepudi@gmail.com>
2019-01-06 00:20:19 -0600

    Initial Files working

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GstAddComponent } from './gst-add/gst-add.component';
+import { GstGetComponent } from './gst-get/gst-get.component';
+import { GstEditComponent } from './gst-edit/gst-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GstAddComponent,
+    GstGetComponent,
+    GstEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/gst-add/gst-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gst-add',
+  templateUrl: './gst-add.component.html',
+  styleUrls: ['./gst-add.component.css']
+})
+export class GstAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
